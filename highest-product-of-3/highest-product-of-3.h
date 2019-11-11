6cb7789dd88c64bd84e3e20011b15ac472b66aa5
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-11 15:36:11 -0600

    Create highest-product-of-3.h

---
+++ highest-product-of-3/highest-product-of-3.h
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+int highestProductOf3(const int *arrayOfInts, size_t length)
+{
+    // calculate the highest product of three numbers
+    
+
+    return 0;
+}
