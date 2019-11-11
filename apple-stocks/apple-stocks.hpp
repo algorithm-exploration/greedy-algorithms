2bf61b4fe7606c56a3d45653ac02e4e95932f680
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-11 15:31:06 -0600

    Create apple-stocks.hpp

---
+++ apple-stocks/apple-stocks.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+int getMaxProfit(const vector<int>& stockPrices)
+{
+    // calculate the max profit
+    
+
+    return 0;
+}
