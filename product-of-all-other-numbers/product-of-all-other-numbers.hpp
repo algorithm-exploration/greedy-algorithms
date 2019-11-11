60f2cdaffa2482ef40cc8f8b4dd7e42e52c33720
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-11 15:38:17 -0600

    Create product-of-all-other-numbers.hpp

---
+++ product-of-all-other-numbers/product-of-all-other-numbers.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+vector<int> getProductsOfAllIntsExceptAtIndex(const vector<int>& intVector)
+{
+    // make a vector with the products
+    
+
+    return vector<int>(intVector.size());
+}
