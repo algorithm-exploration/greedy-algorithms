313aa1543797d9b0b30488832d06a2501932ebd0
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-11 15:39:27 -0600

    Create product-of-all-other-numbers.h

---
+++ product-of-all-other-numbers/product-of-all-other-numbers.h
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+void getProductsOfAllIntsExceptAtIndex(
+    const int *intArray,
+    size_t intArrayLength,
+    int *productsOfAllIntsExceptAtIndexOutput,
+    size_t productsOfAllIntsExceptAtIndexOutputLength)
+{
+    // fill in productsOfAllIntsExceptAtIndexOutput with the products
+    
+
+}
