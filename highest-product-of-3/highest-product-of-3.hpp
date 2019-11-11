fe066261227a5229c2a2fe7b029504f23144b765
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-11 15:37:19 -0600

    Create highest-product-of-3.hpp

---
+++ highest-product-of-3/highest-product-of-3.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+int highestProductOf3(const vector<int>& vectorOfInts)
+{
+    // calculate the highest product of three numbers
+    
+
+    return 0;
+}
