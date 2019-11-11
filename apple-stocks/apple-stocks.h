00725e73640a4b44fc80cfba0852d885ce742a72
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-11 15:31:51 -0600

    Create apple-stocks.h

---
+++ apple-stocks/apple-stocks.h
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+int getMaxProfit(const int *stockPrices, size_t length)
+{
+    // calculate the max profit
+    
+
+    return 0;
+}
+
